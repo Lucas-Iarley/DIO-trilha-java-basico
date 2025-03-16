1c8337244095d16b452d42a149f688bd1912e7b7
Lucas Iarley <94692521+Lucas-Iarley@users.noreply.github.com>
2025-03-16 17:15:47 -0300

    Update TiposeVariaveis.java

--- TiposeVariaveis.java
+++ TiposeVariaveis.java
@@ -1,6 +1,17 @@
 public class TiposeVariaveis {
 	public static void main(String[] args) {
 
+		/*
+		existem dois tipos de dados
+		byte - 1 byte de memória
+		short - 2 byte de memoria
+		int 4 bytes de memoria
+		long - 8 bytes de memoria
+
+		o JDK sempre tenta converter para int dos dados de 2 ou menos de bytes
+		
+
+	*/
 		System.out.println("olá");
 		
 	}
