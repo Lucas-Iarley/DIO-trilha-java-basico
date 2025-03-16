05904d58b82c3a76919a63c4cf9558d5df6c650b
Lucas Iarley <94692521+Lucas-Iarley@users.noreply.github.com>
2025-03-16 18:52:02 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores{
+
+	public static void main(String[] args) {
+		// operadores aritmetricos
+
+		double soma = 10.5 + 15.01;
+		int subtracao = 10 - 5;
+		int multiplicacao = 10 * 10 ;
+		int divisao = 10/2;
+		int modulo = 18% 3;
+		double resultado = (10*7)+ (20/4);
+
+		System.out.println(resultado);
+		System.out.println(modulo);
+
+		// 	concatenação
+		String nomeCompleto = "Linguagem " + "JAVA";
+		System.out.println(nomeCompleto);
+
+		// incrementação
+		int numero = 5;
+		numero = numero +5;
+		System.out.println(numero);
+	}
+}
